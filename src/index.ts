5fe2100e90a20e82a3203fba5080d9e792a00dae
Dokja <revanamike620@gmail.com>
2025-01-27 11:47:29 +0300

    ✨ feat: implement Sass compiler with watch mode and build scripts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env node
+
+import { SassCompiler } from "./compiler";
+import path from "path";
+
+const args = process.argv.slice(2);
+const isWatchMode = args.includes("--watch");
+
+const compiler = new SassCompiler({
+  scssDir: path.join(process.cwd(), "scss"),
+  outputDir: path.join(process.cwd(), "src"),
+  mainFile: path.join(process.cwd(), "scss/global.scss"),
+  sourceMap: true,
+});
+
+if (isWatchMode) {
+  compiler.watch();
+} else {
+  compiler.compile();
+}
